4f5988c2a2fb29d27664c29e581ac6c574a4fe6a
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2021-05-11 09:44:15 +0100

    added redux store

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,15 +1,15 @@
-const notFound = (req, res, net) => {
-	const error = new Error('Not Found - ${req.oringinalUrl');
+const notFound = (req, res, next) => {
+	const error = new Error(`Not Found - ${req.originalUrl}`);
 	res.status(404);
 	next(error);
 };
 
 const errorHandler = (err, req, res, next) => {
-	const statusCode = res.statusCode === 200 ? 500 : res.StatusCode;
+	const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
 	res.status(statusCode);
 	res.json({
 		message: err.message,
-		stavk: process.env.NODE_ENV === 'production' ? null : err.stack
+		stack: process.env.NODE_ENV === 'producton' ? null : err.stack
 	});
 };
 
